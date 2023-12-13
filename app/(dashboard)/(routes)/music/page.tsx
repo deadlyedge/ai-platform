aa9d87c7a735559558e1d0c878654a12d257b2f6
xdream oldlu <xdream@gmail.com>
2023-12-13 18:15:06 +0800

    and the music works

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,108 @@
+"use client"
+
+import axios from "axios"
+import * as z from "zod"
+import { Music } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+import Heading from "@/components/heading"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Empty } from "@/components/empty"
+import { Loader } from "@/components/loader"
+
+import { formSchema } from "./constants"
+
+type MusicPageProps = {}
+
+export default function MusicPage() {
+  const router = useRouter()
+  const [music, setMusic] = useState<string>()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined)
+
+      const response = await axios.post("/api/music", values)
+
+      setMusic(response.data.audio)
+
+      form.reset()
+    } catch (error) {
+      // TODO: Open Pro Modal
+      console.log(error)
+    } finally {
+      router.refresh()
+    }
+  }
+
+  return (
+    <div>
+      <Heading
+        title='Music'
+        description='根据提示生成音乐'
+        icon={Music}
+        iconColor='text-emerald-500'
+        bgColor='bg-emerald-500/10'
+      />
+      <div className='px-4 lg:px-8'>
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2'>
+              <FormField
+                name='prompt'
+                render={({ field }) => (
+                  <FormItem className='col-span-12 lg:col-span-10'>
+                    <FormControl className='m-0 p-0'>
+                      <Input
+                        className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
+                        disabled={isLoading}
+                        placeholder='A guitar solo about moutains and springs'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className='col-span-12 lg:col-span-2 w-full'
+                disabled={isLoading}
+                type='submit'
+                size='icon'>
+                生成
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className='mt-4 space-y-4'>
+          {isLoading && (
+            <div className='p-8 rounded-lg w-full flex items-center justify-center bg-muted'>
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && <Empty label='还没有音乐' />}
+          {music && (
+            <audio controls loop autoPlay={true} className='w-full'>
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
